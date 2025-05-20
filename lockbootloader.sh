0652affd95549490a989c26242248b386622bf11
gates-alt <gatestechalt@outlook.com>
2025-05-20 19:54:25 -0400

    path fixed

--- lockbootloader.sh
+++ lockbootloader.sh
@@ -4,5 +4,5 @@ while true; do
     termux-usb -l > file.txt || continue
     USB_DEVICE=$(grep -o /dev/bus/usb/[0-9]*/[0-9]* file.txt)
     termux-usb -r $USB_DEVICE || continue
-    termux-usb -e "/spd_dump --usb-fd loadexec bin/custom_exec_no_verify_65015f08.bin fdl bin/fdl1-dl.bin 0x65000800 fdl bin/fdl2-dl.bin 0x9efffe00 exec wof miscdata 8192 zero.bin verity 1 w misc misc-wipe.bin reset" $USB_DEVICE || continue
+    termux-usb -e "/spd_dump --usb-fd loadexec bin/custom_exec_no_verify_65015f08.bin fdl bin/fdl1-dl.bin 0x65000800 fdl bin/fdl2-dl.bin 0x9efffe00 exec wof miscdata 8192 bin_misc/zero.bin verity 1 w misc misc-wipe.bin reset" $USB_DEVICE || continue
 done
