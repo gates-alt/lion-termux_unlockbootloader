86c451390dffdd81e42877faa8403abee182159d
gates-alt <gatestechalt@outlook.com>
2025-05-20 16:53:50 -0400

    simple script added

---
+++ connect.sh
@@ -0,0 +1,8 @@
+termux-wake-lock
+
+while true; do
+    termux-usb -l > file.txt || continue
+    USB_DEVICE=$(grep -o /dev/bus/usb/[0-9]*/[0-9]* file.txt)
+    termux-usb -r $USB_DEVICE || continue
+    termux-usb -e "/spd_dump --usb-fd loadexec bin/custom_exec_no_verify_65015f08.bin fdl bin/fdl1-dl.bin 0x65000800 fdl bin/fdl2-dl.bin 0x9efffe00 exec" $USB_DEVICE || continue
+done
